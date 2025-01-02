6505d96b260674acf940b7ac402562337ba12f53
Nimesh <nimeshstha79@gmail.com>
2025-01-02 22:04:57 +0545

    update app with homepage

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
+import  HomePage  from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
-
+import Message from './pages/Message';
+import  { Trynow } from './pages/Trynow';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/Contactus" element={<Message />} />
+        <Route path="/Trynow" element={<Trynow />} />
       </Routes>
     </BrowserRouter>
   );
