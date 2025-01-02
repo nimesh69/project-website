d7cd9248dbb31af1413fdaf400586427defc58dc
Nimesh <nimeshstha79@gmail.com>
2025-01-02 22:05:18 +0545

    linked homepage

--- project/src/components/Header/NavButtons.tsx
+++ project/src/components/Header/NavButtons.tsx
@@ -6,15 +6,14 @@ export function NavButtons() {
   return (
     <div className="flex items-center gap-6">
       <nav className="hidden md:flex items-center gap-6">
-        <Link href="#">Home</Link>
-        <Link href="/">Try Now</Link>
+        <Link href="/">Home</Link>
         <Link href="#">Our Work</Link>
         <Link href="#">About us</Link>
       </nav>
       <Link href="/login" className="p-2 hover:bg-gray-100 rounded-full">
         <LogIn className="w-4 h-4 ml-2" />
       </Link>
-      <Link href="Contactus" className="p-2 hover:bg-gray-100 rounded-full">
+      <Link href="/Contactus" className="p-2 hover:bg-gray-100 rounded-full">
         <LucideMessagesSquare className="w-5 h-5" />
       </Link>
     </div>
