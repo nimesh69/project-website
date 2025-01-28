314ec5e999587ee5b4ba0f3d58d901bb9d6a1f70
Nimesh Shrestha <107875978+nimesh69@users.noreply.github.com>
2025-01-28 19:41:41 +0545

    model link update

--- project/main.py
+++ project/main.py
@@ -38,7 +38,7 @@ app.add_middleware(
 # Model URLs
 MODEL_URLS = {
     'realesr-general-x4v3.pth': "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.5.0/realesr-general-x4v3.pth",
-    'GFPGANv1.4.pth': "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth",
+    'GFPGANv1.4.pth': "https://github.com/nimesh69/Recovering-and-Enhancing-Digital-Facial-Images-/releases/download/v6.0/GFPGANv1.4.pth",
     # 'GFPGANv1.4.pth': "https://github.com/nimesh69/Recovering-and-Enhancing-Digital-Facial-Images-/releases/download/v3.0/asian+celebx+ffhq.pth",
 }
 
